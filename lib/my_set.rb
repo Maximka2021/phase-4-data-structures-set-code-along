48733ba7199887daadaaa737502d64d0f66ecbbe
Maxim <polyakovmaxim05@gmail.com>
2022-12-16 09:21:56 -0500

    commit tree be38824ebecfdc0d7591e015d11534a23d58f4b3

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,12 @@
-# your code here
+class MySet
+    def initialize enumerable = []
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include? value
+        @hash.has_key?(value)
+    end
+end
